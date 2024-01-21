5dad1f2752cb9b6c3d6e8b3cc50c8d6676eddd46
Kolade-dotcom <109832012+Kolade-dotcom@users.noreply.github.com>
2024-01-21 14:00:42 +0000

    Sun, Jan 21, 2024, 3:00 PM +01:00

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,23 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
-// import Categories from "./pages/Categories/Categories";
+import React from "react";
 
 function App() {
-  return (
-    // <Router>
-    //   <Box sx={{
-    //     bgcolor: (theme) => {theme.palette.background.default},
-    //     minHeight: "100vh",
-    //   }}>
-    //     <Routes>
-    //       <Route path="/categories" element={<Categories />} />
-    //     </Routes>
-    //   </Box>
-    // </Router>
-
-    <div>App</div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
 
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Box } from "@mui/material";
+// import Categories from "./pages/Categories/Categories";
+
+// <Router>
+//   <Box sx={{
+//     bgcolor: (theme) => {theme.palette.background.default},
+//     minHeight: "100vh",
+//   }}>
+//     <Routes>
+//       <Route path="/categories" element={<Categories />} />
+//     </Routes>
+//   </Box>
+// </Router>
