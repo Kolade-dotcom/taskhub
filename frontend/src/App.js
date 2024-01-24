1a2a8758848ad1a867fa6db34742e56dea52f039
Kolade-dotcom <109832012+Kolade-dotcom@users.noreply.github.com>
2024-01-24 07:22:27 +0000

    Wed, Jan 24, 2024, 8:22 AM +01:00

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <CssBaseline />
+      Hello
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
