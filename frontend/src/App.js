3e281fe9e753747da72a461e0807111fd202cb34
Kolade-dotcom <109832012+Kolade-dotcom@users.noreply.github.com>
2024-01-24 07:26:04 +0000

    Wed, Jan 24, 2024, 8:26 AM +01:00

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Categories from "src/pages/Categories/Categories";
 
 export default function App() {
   return (
     <div>
       <CssBaseline />
-      Hello
+      <Router>
+        <Box
+          sx={{
+            bgcolor: (theme) => {
+              theme.palette.background.default;
+            },
+            minHeight: "100vh",
+          }}
+        >
+          <Routes>
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </Box>
+      </Router>
     </div>
   );
 }
