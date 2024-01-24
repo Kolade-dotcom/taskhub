510204fb8faa8925f84e99bca3f5a708dc5e5897
Kolade-dotcom <109832012+Kolade-dotcom@users.noreply.github.com>
2024-01-24 07:14:35 +0000

    Wed, Jan 24, 2024, 8:14 AM +01:00

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
-import React from "React";
+import React from "react";
+import ReactDOM from "react-dom";
 
 export default function App() {
   return <div>Hello</div>;
 }
+
+ReactDOM.render(<App />, document.getElementById("root"))
